376e2e214c13784d8a3d5afee44f17f79b939f99
Oteri Eyenike <teyenike1@gmail.com>
2022-05-02 20:24:12 +0100

    Add: configuration and setup of project

---
+++ components/Blog.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="excerpt">
+    <p>The idea is to have a small element with absolute-sized backgrounds placed in different relative positions (e.g.,
+      using percentages). The use of absolute and relative is essential here. Having absolute-sized backgrounds means
+      that their size will not change depending on the size of the container, but their position is relative, so it will
+      change (or give the impression of changing) when the container is resized.
+
+      The relative distance between the dots is the same at all times. But as the container grows, the absolute distance
+      between them expands too-similarly to how real fireworks work.</p>
+  </div>
+</template>
+
+<script></script>
---
+++ nuxt.config.js
@@ -0,0 +1,53 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: "static",
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: "Learn Programming as a Nomad",
+    htmlAttrs: {
+      lang: "en",
+    },
+    meta: [
+      {charset: "utf-8"},
+      {name: "viewport", content: "width=device-width, initial-scale=1"},
+      {hid: "description", name: "description", content: "A blog that explores the new norm of working from anywhere as long as internet is available. Work begins with you (freedom)"},
+      {name: "format-detection", content: "telephone=no"},
+    ],
+    link: [{rel: "icon", type: "image/x-icon", href: "/favicon.ico"},
+      {
+        rel: "preconnect", href: "https://fonts.googleapis.com"
+      },
+      {
+        rel: "preconnect", href: "https://fonts.gstatic.com"
+      },
+      {
+        href: "https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap",
+        rel: "stylesheet"
+      }
+    ],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: ["@nuxt/content"],
+
+  // Modules: https://content.nuxtjs.org/
+  content: {
+    // nuxt content options
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+};
